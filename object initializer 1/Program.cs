e2a37031d8901bc419a092537ca8f6bb1fafd74c
S3gillu <shubhamsaurabh440@yahoo.com>
2017-09-27 15:37:23 +0530

    This is object initializer

---
+++ object initializer 1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace object_initializer_1
+{
+    public class Car
+    {
+        public string CarName { get; set; }
+        public string Color { get; set; }
+
+        public CarManufacturer Manufacturer { get; set; }
+        public string CompanyName { get; set; }
+        public string Country { get; set; }
+
+    }
+
+    public class CarManufacturer
+    {
+        public static void Main()
+        {
+            Car car = new Car
+            {
+                CarName = "punto",
+                Color = "Black",
+                CompanyName = "Fiat",
+                Country = "USA"
+
+
+            };
+            Console.WriteLine($" CarName:{car.CarName}");
+            Console.WriteLine($" Color:{car.Color}");
+            Console.WriteLine($" CompanyName:{car.CompanyName}");
+            Console.WriteLine($" Country:{car.Country}");
+            Console.ReadLine();
+         }
+
+   
+    }
+
+}
+
+
